4f605747f052a5f4689181fb5373d780bcef48be
litmus-zhang <40471908+litmus-zhang@users.noreply.github.com>
2022-06-02 12:48:49 +0100

    Completed the simple mern app

--- src/App.js
+++ src/App.js
@@ -1,23 +1,54 @@
 import logo from './logo.svg';
 import './App.css';
+import Axios from 'axios';
+import { useState, useEffect } from "react"
 
-function App() {
+function App()
+{
+  const [listOfUsers, setListOfUsers] = useState([]);
+  const [name, setName] = useState("");
+  const [age, setAge] = useState(0);
+  const [username, setUserName] = useState("");
+
+  const createUser = async () =>
+  {
+    Axios.post("http://localhost:3001/createUser",
+      {  name, age, username }).then(response =>
+    {
+        alert("User created successfully");
+        setListOfUsers([...listOfUsers, response.data]);
+    })
+  }
+
+
+  useEffect(() =>
+  {
+    Axios.get("http://localhost:3001/getUsers").then(response =>
+    {
+      setListOfUsers(response.data)
+    })
+  }, [listOfUsers]);
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="userDisplay">
+        {
+          listOfUsers.map(user =>
+          { 
+            return <div>
+              <h1>Name: {user.name}</h1>
+              <h1>Age: {user.age}</h1>
+              <h1>Username: {user.username}</h1>
+            </div>
+          })
+        }
+      </div>
+      <div>
+        <input name='' type="text" placeholder="Name..." onChange={(e)=> setName(e.target.value)}/>
+        <input type="number" placeholder="Age..." onChange={(e)=> setAge(e.target.value)}/>
+        <input type="text" placeholder="Username..." onChange={(e)=> setUserName(e.target.value)}/>
+        <button onClick={createUser}>Create User</button>
+      </div>
     </div>
   );
 }
